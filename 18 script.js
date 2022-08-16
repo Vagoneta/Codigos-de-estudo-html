06ae34cd57504ab8f23a3463181fc02bf6f784a0
Vagoneta <103613467+Vagoneta@users.noreply.github.com>
2022-08-16 19:11:14 -0300

    Add files via upload

---
+++ 18 script.js
@@ -0,0 +1,39 @@
+var primeiroNumero;
+var segundoNumero;
+var soma;
+var media;
+
+primeiroNumero = 10;
+segundoNumero = 7;
+soma = primeiroNumero + segundoNumero;
+media = soma / 2;
+
+console.log(primeiroNumero);
+console.log(segundoNumero);
+console.log(soma);
+console.log(media);
+console.log(primeiroNumero * segundoNumero);
+
+let texto1 = "Esse é o meu primeiro texto";
+var texto2 = "Esse é o meu segundo texto";
+const texto3 = "Esse é o meu terceiro texto";
+
+console.log(texto1 + texto2 + texto3);
+
+let indicador = 1;
+
+console.log(indicador + texto1)
+
+let x = 9;
+let y = 9;
+
+if(x > y){
+	alert("X é maior que Y");
+}
+if(y > x){
+	alert("Y é maior que X");
+}
+if(x == y){
+	alert("X é igual a Y");
+}
+
